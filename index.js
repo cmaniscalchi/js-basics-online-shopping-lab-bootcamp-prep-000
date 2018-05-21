b99faa5fb676691f7934a054b47f33a713350258
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-05-21 02:55:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,8 +70,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+var myCost = total();
+
+
   if (cardNumber !== undefined) {
-  return `Your total cost is ${cost}, which will be charged to the card ${cardNumber}.`;
+  return `Your total cost is ${myCost}, which will be charged to the card ${cardNumber}.`;
   }
   else return `Sorry, we don't have a credit card on file for you.`;
 }
